3aab2658e81a9d3ec78bc2b30371713eb83e6a7b
samurailance <linwinpei123@gmail.com>
2022-09-20 20:40:57 +0900

    second commit

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -3,10 +3,13 @@
 @section('content')
 
 <div class="section-1 text-center content-banner">
-<h1>About Us</h1>
+  <h1>About Us</h1>
 
-<p class="py-3">FriendQuiz is a fun website where you can check how much your friends know about you with our super fun friendship quizzes hand-crafted for all our users. FriendsForever lets you have a great time with your friends and make your bond even stronger. 
-Standing separate from our competitors, FriendsForever provides different vibrant, fun, and engaging quizzes. So, make your quiz, share with your friends ,and test your friendship now!</p>
+  <p class="py-3">FriendQuiz is a funs website where you can check how much your friends know about you with our super
+    fun friendship quizzes hand-crafted for all our users. FriendsForever lets you have a great time with your friends
+    and make your bond even stronger.
+    Standing separate from our competitors, FriendsForever provides different vibrant, fun, and engaging quizzes. So,
+    make your quiz, share with your friends ,and test your friendship now!</p>
 </div>
 
 @stop
